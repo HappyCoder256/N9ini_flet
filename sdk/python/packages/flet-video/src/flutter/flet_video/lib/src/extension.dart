3d36ee69336f1f75f737238a7522ee4bd358e258
HappyCoder256 <shamndohjovy256@gmail.com>
2026-03-27 10:35:07 +0300

    Refactor Extension class for video control handling

--- sdk/python/packages/flet-video/src/flutter/flet_video/lib/src/extension.dart
+++ sdk/python/packages/flet-video/src/flutter/flet_video/lib/src/extension.dart
@@ -3,25 +3,16 @@ import 'package:flutter/cupertino.dart';
 import 'package:media_kit/media_kit.dart';
 import 'video.dart';
 
-class Extension extends FletExtension {
-  @override
-  void ensureInitialized() {
-    MediaKit.ensureInitialized();
+CreateControlFactory createControl = (CreateControlArgs args) {
+  switch (args.control.type) {
+    case "video":
+      return VideoControl(
+          parent: args.parent, control: args.control, backend: args.backend);
+    default:
+      return null;
   }
+};
 
-  @override
-  Widget? createWidget(Key? key, Control control, Control? parent,
-      bool parentDisabled, bool? parentAdaptive, FletControlBackend backend) {
-    switch (control.type) {
-      case "Video":
-        return VideoControl(
-          key: key,
-          parent: parent,
-          control: control,
-          backend: backend,
-        );
-      default:
-        return null;
-    }
-  }
+void ensureInitialized() {
+  MediaKit.ensureInitialized();
 }
