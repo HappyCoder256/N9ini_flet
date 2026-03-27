7eab19623ce4a270cde6575ce4581fc094d0db14
HappyCoder256 <shamndohjovy256@gmail.com>
2026-03-27 10:48:22 +0300

    Comment out unused code in extension.dart
    Comment out unused imports and class definition in extension.dart

--- sdk/python/packages/flet-video/src/flutter/flet_video/lib/src/extension.dart
+++ sdk/python/packages/flet-video/src/flutter/flet_video/lib/src/extension.dart
@@ -16,3 +16,31 @@ CreateControlFactory createControl = (CreateControlArgs args) {
 void ensureInitialized() {
   MediaKit.ensureInitialized();
 }
+
+// import 'package:flet/flet.dart';
+// import 'package:flutter/cupertino.dart';
+// import 'package:media_kit/media_kit.dart';
+// import 'video.dart';
+
+// class Extension extends FletExtension {
+//   @override
+//   void ensureInitialized() {
+//     MediaKit.ensureInitialized();
+//   }
+
+//   @override
+//   Widget? createWidget(Key? key, Control control, Control? parent,
+//       bool parentDisabled, bool? parentAdaptive, FletControlBackend backend) {
+//     switch (control.type) {
+//       case "Video":
+//         return VideoControl(
+//           key: key,
+//           parent: parent,
+//           control: control,
+//           backend: backend,
+//         );
+//       default:
+//         return null;
+//     }
+//   }
+// }
