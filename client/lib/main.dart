398afd76f736c29e6d7aecedf815bef5a4f68126
HappyCoder256 <shamndohjovy256@gmail.com>
2026-03-28 23:03:18 +0300

    Update main.dart

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -10,7 +10,7 @@ import 'package:flet_audio_recorder/flet_audio_recorder.dart'
 import "package:flet_flashlight/flet_flashlight.dart" as flet_flashlight;
 import 'package:flet_geolocator/flet_geolocator.dart' as flet_geolocator;
 import 'package:flet_lottie/flet_lottie.dart' as flet_lottie;
-// import 'package:flet_carousel_slider/flet_carousel_slider.dart' as flet_carousel_slider;
+import 'package:flet_carousel_slider/flet_carousel_slider.dart' as flet_carousel_slider;
 import 'package:flet_map/flet_map.dart' as flet_map;
 import 'package:flet_permission_handler/flet_permission_handler.dart'
     as flet_permission_handler;
@@ -42,7 +42,7 @@ void main([List<String>? args]) async {
   flet_audio_recorder.ensureInitialized();
   flet_geolocator.ensureInitialized();
   flet_permission_handler.ensureInitialized();
-  // flet_carousel_slider.ensureInitialized();
+  flet_carousel_slider.ensureInitialized();
   flet_lottie.ensureInitialized();
   flet_map.ensureInitialized();
   flet_ads.ensureInitialized();
@@ -113,7 +113,7 @@ void main([List<String>? args]) async {
 // --FAT_CLIENT_END--
       flet_audio_recorder.createControl,
       flet_geolocator.createControl,
-      // flet_carousel_slider.createControl,
+      flet_carousel_slider.createControl,
       flet_permission_handler.createControl,
       flet_lottie.createControl,
       flet_map.createControl,
